bedb324f0158dfcd1929291763eebd133bcd034c
PeterLiu001 <PeterLiu@poweronlabs.com>
2024-09-30 12:20:22 +0800

    12

--- application.542a4.js
+++ application.542a4.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.6d635.json';
+          this.settingsPath = 'src/settings.7a685.json';
           this.showFPS = true;
         }
         _createClass(Application, [{
--- index.0bed0.js
+++ index.0bed0.js
@@ -1,4 +1,4 @@
-System.register(["./application.d4b6c.js"], function (_export, _context) {
+System.register(["./application.542a4.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
