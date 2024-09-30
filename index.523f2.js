d9a47c3c02e1be7f36dfeb290da69732420f279f
PeterLiu001 <PeterLiu@poweronlabs.com>
2024-09-30 14:20:25 +0800

    123

--- index.523f2.js
+++ index.523f2.js
@@ -1,4 +1,4 @@
-System.register(["./application.542a4.js"], function (_export, _context) {
+System.register(["./application.bdd13.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
